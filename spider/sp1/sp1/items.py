cc3e184e10aa9e5c24b9f6a9c5a0e58f1db0b751
brady.wang <brady.wang@qq.com>
2018-04-26 21:34:06 +0800

    7160

--- spider/sp1/sp1/items.py
+++ spider/sp1/sp1/items.py
@@ -10,5 +10,6 @@ import scrapy
 
 class Sp1Item(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    title = scrapy.Field()
+    img_url = scrapy.Field()
     pass
--- spider/sp1/sp1/spiders/wang.py
+++ spider/sp1/sp1/spiders/wang.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from time import sleep
+
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
@@ -7,15 +9,23 @@ from scrapy.spiders import CrawlSpider, Rule
 class WangSpider(CrawlSpider):
     name = 'wang'
     allowed_domains = ['www.7160.com']
-    start_urls = ['http://www.7160.com/']
+    start_urls = ['http://www.7160.com']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=r'http://www.7160.com.*'), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
         i = {}
-        #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
+
+        title_list = response.xpath('/html/body/div/div[2]/div[1]/div[2]/h1/text()').extract()
+        img_url_list = response.xpath('/html/body/div/div[2]/div[1]/div[2]/div[3]/p/a/img/@src').extract()
+        print("***************************************************************")
         #i['name'] = response.xpath('//div[@id="name"]').extract()
         #i['description'] = response.xpath('//div[@id="description"]').extract()
-        return i
+        if(len(title_list) > 0 and len(img_url_list) > 0 ):
+            i['title'] = title_list[0]
+            i['img_url'] = img_url_list[0]
+            yield i
+        else:
+            print("不是需要的页面"+response.url)
